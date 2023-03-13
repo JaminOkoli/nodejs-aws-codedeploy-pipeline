b60e3d367b2d135ac066af298bd8b59808acafa7
Okoli, Benjamin <jaminokoli10@gmail.com>
2023-03-13 17:28:26 +0100

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -4,5 +4,5 @@ echo 'run application_start.sh: ' >> /home/ec2-user/nodejs-aws-codedeploy-pipeli
 
 echo 'pm2 restart nodejs-express-app' >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
 npm install pm2 -g
-pm2 start nodejs-express-app >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
+pm2 start nodejs-express-app
 pm2 restart nodejs-express-app >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
