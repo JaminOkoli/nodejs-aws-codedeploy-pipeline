3caf74a18998e18f7552032ed358776386d0342c
Okoli, Benjamin <jaminokoli10@gmail.com>
2023-03-13 17:00:21 +0100

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -4,5 +4,6 @@ echo 'run after_install.sh: ' >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/d
 echo 'cd /home/ec2-user/nodejs-server-cicd' >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
 cd /home/ec2-user/nodejs-aws-codedeploy-pipeline >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
 sudo apt install nodejs -y
+sudo apt install npm -y
 echo 'npm install' >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log 
 npm install >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
