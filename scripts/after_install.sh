f784747d6e77c47d76c85bb3dd42ae35aea88205
Okoli, Benjamin <jaminokoli10@gmail.com>
2023-03-14 12:21:13 +0100

    Fix nodejs ish

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -4,4 +4,8 @@ echo 'run after_install.sh: ' >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/d
 echo 'cd /home/ec2-user/nodejs-server-cicd' >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
 cd /home/ec2-user/nodejs-aws-codedeploy-pipeline >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
 echo 'npm install' >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log 
-npm install >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
+sudo apt install curl dirmngr apt-transport-https lsb-release ca-certificates
+curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+sudo apt install nodejs
+
+# npm install >> /home/ec2-user/nodejs-aws-codedeploy-pipeline/deploy.log
